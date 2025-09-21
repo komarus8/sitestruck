2516222e541ab50b82e82b20d9ca7c665ca5bc70
komarus8 <crafterkuba11@gmail.com>
2025-09-21 21:09:11 +0200

    Update generator.js

--- myapp/backend/routes/generator.js
+++ myapp/backend/routes/generator.js
@@ -1,1 +1,11 @@
+const express = require("express");
+const router = express.Router();
+
+// tutaj tylko atrapa generowania zipa
+router.post("/", (req, res) => {
+  const { username, template } = req.body;
+  res.json({ success: true, file: `${template}-site.zip` });
+});
+
+module.exports = router;
 
