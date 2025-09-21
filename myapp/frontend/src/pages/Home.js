4e67b69f76df1bc21c778fa3d74ea0357d1da370
komarus8 <crafterkuba11@gmail.com>
2025-09-21 21:21:58 +0200

    Update Home.js

--- myapp/frontend/src/pages/Home.js
+++ myapp/frontend/src/pages/Home.js
@@ -1,1 +1,13 @@
+import React from "react";
+import ReviewSection from "../components/ReviewSection";
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Welcome to Font Generator</h1>
+      <p>Buy a plan, generate your site, and manage it easily!</p>
+      <ReviewSection />
+    </div>
+  );
+}
 
