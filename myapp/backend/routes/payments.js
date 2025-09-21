19314f46dbabec35176ad971c329257739000849
komarus8 <crafterkuba11@gmail.com>
2025-09-21 21:08:40 +0200

    Update payments.js

--- myapp/backend/routes/payments.js
+++ myapp/backend/routes/payments.js
@@ -1,1 +1,11 @@
+const express = require("express");
+const router = express.Router();
+
+// na razie atrapa płatności
+router.post("/checkout", (req, res) => {
+  const { username, plan } = req.body;
+  res.json({ success: true, message: `Fake payment for ${plan} done` });
+});
+
+module.exports = router;
 
