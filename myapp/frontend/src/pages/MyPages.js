450dd6b5fec0fcbdd6c8b69a644bf505debe712c
komarus8 <crafterkuba11@gmail.com>
2025-09-21 21:23:13 +0200

    Update MyPages.js

--- myapp/frontend/src/pages/MyPages.js
+++ myapp/frontend/src/pages/MyPages.js
@@ -1,1 +1,12 @@
+import React from "react";
+
+export default function MyPages() {
+  return (
+    <div>
+      <h2>My Pages</h2>
+      <p>Here user will see their generated websites.</p>
+      <button>Buy New Page</button>
+    </div>
+  );
+}
 
