1908e004a93557eb47d53cd02b14c697c98cbdc1
komarus8 <crafterkuba11@gmail.com>
2025-09-21 21:20:50 +0200

    Update App.js

--- myapp/frontend/src/App.js
+++ myapp/frontend/src/App.js
@@ -1,1 +1,27 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import MyPages from "./pages/MyPages";
+import Generator from "./pages/Generator";
+
+function App() {
+  return (
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/mypages" element={<MyPages />} />
+        <Route path="/generator" element={<Generator />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
 
