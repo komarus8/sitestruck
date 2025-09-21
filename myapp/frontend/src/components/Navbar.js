b8f495c196235e3d592cb5c23d7ff23335c3e83a
komarus8 <crafterkuba11@gmail.com>
2025-09-21 21:21:12 +0200

    Update Navbar.js

--- myapp/frontend/src/components/Navbar.js
+++ myapp/frontend/src/components/Navbar.js
@@ -1,1 +1,15 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+export default function Navbar() {
+  return (
+    <nav className="navbar">
+      <Link to="/">Home</Link>
+      <Link to="/generator">Generator</Link>
+      <Link to="/mypages">My Pages</Link>
+      <Link to="/login">Login</Link>
+      <Link to="/register">Register</Link>
+    </nav>
+  );
+}
 
